0cc25d4508776c9b1673944a5b27b3f934a3f8a9
Mahabub Alam Khan <85025273+Mahabub1727@users.noreply.github.com>
2021-08-18 13:55:17 +0600

    Add files via upload

---
+++ Lab Task 7/header2.php
@@ -0,0 +1,32 @@
+<?php
+	echo '
+	
+
+	<fieldset >
+
+	    <p align="left" >
+	    	
+
+	        <a href="../view/homepageManager.php" style="font-size:25px" >Home</a>&nbsp;&nbsp;&nbsp;
+	        <a href="../view/myProfile.php" style="font-size:25px" >Profile</a>&nbsp;&nbsp;&nbsp;
+	         <a href="../view/editProfile.php" style="font-size:25px" >Edit Profile</a>&nbsp;&nbsp;&nbsp;
+	        <a href="../controller/addEmployee.php" style="font-size:25px" >Add Employee</a>&nbsp;&nbsp;&nbsp;
+	        <a href="../view/showEmployee.php" style="font-size:25px" >Show Employee</a>&nbsp;&nbsp;&nbsp;
+	        <a href="../view/searchEmployee.php" style="font-size:25px" >Search Employee</a>&nbsp;&nbsp;&nbsp;
+	        <a href="../view/deleteEmployee.php" style="font-size:25px" >Delete Employee</a>&nbsp;&nbsp;&nbsp;
+	        
+	        <a href="../view/logout.php" style="font-size:25px" >Log Out</a>
+	        
+	    </p> 
+		<p><br></p>
+	    <center><span style= "color: #00AE29;font-size:40px;"><b>GREEN</span>
+	    <span style= "color:;font-size:40px;"><b>DHAKA</span>
+	    <span style= "color:#547544;font-size:40px;"><b>BUS LIMITED</span>
+
+	    
+
+	    
+	</fieldset>
+	';
+
+?>
